53307d5f8e7f50f9a5b572afc43e8c0cd30437fc
Ashraf-Sameh-ElAasar <126814414+Ashraf-Sameh-ElAasar@users.noreply.github.com>
2023-03-11 15:58:05 +0200

    Add files via upload

---
+++ Projects/I2C_Rec/I2C_Rec/Debug/I2C_Rx_main.d
@@ -0,0 +1,10 @@
+I2C_Rx_main.d I2C_Rx_main.o: .././I2C_Rx_main.c .././Std_Types.h \
+ .././Bit_Math.h .././DIO_interface.h .././I2C_interface.h
+
+.././Std_Types.h:
+
+.././Bit_Math.h:
+
+.././DIO_interface.h:
+
+.././I2C_interface.h:
